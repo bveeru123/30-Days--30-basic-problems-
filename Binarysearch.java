ccd0eff2a5ae355a2118bddd39e6e0e84f21103a
veerashetty <bveeru012@gmail.com.in>
2024-04-17 21:44:24 +0530

    file added

---
+++ Binarysearch.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Binarysearch{
+    public static void main(String args[]) {
+        Scanner sc=new Scanner(System.in );
+    int [] arr={4,3,6,8,6,1,24,6};
+        int key=sc.nextInt();
+   int ans=0;
+   for(int i=0;i<arr.length;i++)
+   {
+       if (arr[i]==key)
+       {
+           ans=i;
+           break;
+       }
+   }
+   System.out.println("element found at index: "  +ans);
+
+    }
+}
