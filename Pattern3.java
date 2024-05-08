534a5fe361d86a6720a33bd33667f987f75aefac
veerashetty <167009823+bveeru123@users.noreply.github.com>
2024-05-08 19:29:52 +0530

    Day 12

--- Pattern3.java
+++ Pattern3.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class Pattern3
 {
     public static void main(String args[]) {
-        Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner( System.in);
         int n = sc.nextInt();
         int m = sc.nextInt();
         diamond(n);
