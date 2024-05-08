06381585f434c5b66001be884a94c10393ae6ce1
veerashetty <167009823+bveeru123@users.noreply.github.com>
2024-05-08 19:16:15 +0530

    Update and rename Pattern_2.java to Pattern3.java

---
+++ Pattern3.java
@@ -0,0 +1,84 @@
+
+import java.util.*;
+public class Pattern3
+{
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        diamond(n);
+        butterfly(n);
+    }
+    public static void diamond(int n)
+    {
+        System.out.println("-----Diamond pattern");
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=n-i;j++)
+            {
+                System.out.print(" ");
+            }
+            int star=(2*i)-1;
+            for(int j=1;j<=star;j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for(int i=n;i>=1;i--)
+        {
+            for(int j=1;j<=n-i;j++)
+            {
+                System.out.print(" ");
+            }
+            int star=(2*i)-1;
+            for(int j=1;j<=star;j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    public static void butterfly(int n)
+    {
+        System.out.println("-----Butterfly pyramid-----");
+            for(int i=1;i<=n;i++)
+            {
+                for(int j=1;j<=i;j++)
+                {
+                    System.out.print("*");
+                }
+                //Spaces
+                int space=2*(n-i);
+                for(int j=1;j<=space;j++)
+                {
+                    System.out.print(" ");
+                }
+                for(int j=1;j<=i;j++)
+                {
+                    System.out.print("*");
+                }
+                System.out.println();
+
+            }
+            for(int i=n;i>=1;i--)
+            {
+                for(int j=1;j<=i;j++)
+                {
+                    System.out.print("*");
+                }
+                //Spaces
+                int space=2*(n-i);
+                for(int j=1;j<=space;j++)
+                {
+                    System.out.print(" ");
+                }
+                for(int j=1;j<=i;j++)
+                {
+                    System.out.print("*");
+                }
+                System.out.println();
+            }
+            System.out.println();
+    }
+}
