48141d2ac2e1a862cdfd1955257fe208afb3a60a
veerashetty <167009823+bveeru123@users.noreply.github.com>
2024-05-09 20:46:51 +0530

    Add files via upload

---
+++ Palindromeofstring.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class Palindromeofstring {
+    static boolean ispalindrome(String s,int i,int j)
+    {
+        if(s.charAt(i)!=s.charAt(j))
+        {
+            return false;
+        }
+        if(j<=i)
+        {
+            return true;
+        }
+        return ispalindrome(s,i+1,j-1);
+    }
+
+
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        String s=sc.next();
+        System.out.println(ispalindrome(s,0,s.length()-1));
+    }
+}
