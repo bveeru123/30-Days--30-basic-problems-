2817116905e628134842cade3cc2bc07d7a2fa0c
veerashetty <167009823+bveeru123@users.noreply.github.com>
2024-05-10 20:05:23 +0530

    Add files via upload

---
+++ Primenumber.java
@@ -0,0 +1,45 @@
+import java.util.*;
+
+public class Primenumber
+{
+    static boolean isprime(int n)
+    {
+        for(int i=2;i*i<=n;i++)
+        {
+            if(n%i==0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+    //method 2
+    static void isprime2(int n)
+    {
+        boolean[] prime=new boolean[n+1];
+        {
+            for(int i=2;i*i<=n;i++) {
+                if (prime[i] == false) {
+                    for (int j = i * i; j <= n; j = j + i) {
+                        prime[j] = true;
+                    }
+                }
+            }
+            for(int i=2;i<=n;i++)
+            {
+                if(prime[i]==false)
+                {
+                    System.out.println(i);
+                }
+            }
+        }
+
+    }
+public static void main(String[] args)
+{
+    Scanner sc=new Scanner(System.in);
+    int n=sc.nextInt();
+    System.out.println(isprime(n));
+    isprime2(n);
+}
+}
