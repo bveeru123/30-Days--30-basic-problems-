fd2a9158ec58b2d8b4b25e3fb74267e169d88cf5
veerashetty <167009823+bveeru123@users.noreply.github.com>
2024-05-10 20:05:59 +0530

    Day 14

--- Primenumber.java
+++ Primenumber.java
@@ -39,7 +39,7 @@ public static void main(String[] args)
 {
     Scanner sc=new Scanner(System.in);
     int n=sc.nextInt();
-    System.out.println(isprime(n));
+    System.out.println( isprime(n));
     isprime2(n);
 }
 }
