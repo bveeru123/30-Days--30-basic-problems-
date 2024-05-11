c647c54d1df9612249c5ca05ae5fe873faedfc04
veerashetty <167009823+bveeru123@users.noreply.github.com>
2024-05-11 23:00:02 +0530

    Add files via upload

---
+++ Pattern4.java
@@ -0,0 +1,42 @@
+import java.util.*;
+public class Pattern4 {
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        //palindrome pattern
+        System.out.println("--------palindrome pattern");
+        for(int i=1;i<=n;i++)
+        {
+            //space
+            for(int j=1;j<=n-i;j++)
+            {
+                System.out.print(" ");
+            }
+            for(int j=i;j>=1;j--)
+            {
+                System.out.print(j);
+            }
+            for(int j=2;j<=i;j++)
+            {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+
+        //trapezium pattern
+        System.out.println("------trapezium pattern");
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=n-i;j++)
+            {
+                System.out.print(" ");
+            }
+            for(int j=1;j<=5;j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
