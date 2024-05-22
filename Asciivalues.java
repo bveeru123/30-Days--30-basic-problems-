e800540b10f87e201e5db15df6275d8a8634dde8
veerashetty <167009823+bveeru123@users.noreply.github.com>
2024-05-22 19:56:25 +0530

    Day 25

---
+++ Asciivalues.java
@@ -0,0 +1,12 @@
+public class Asciivalues {
+    public static void main(String args[])
+    {
+        char ch;
+        int i;
+        for(ch='A';ch<='Z';ch++)
+        {
+            i=ch;
+            System.out.println("character = "+ch+" Ascii value ="+i);
+        }
+    }
+}
