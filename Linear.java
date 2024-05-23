17d229b4b81c011be91524b7ee887f97aabeb0a3
veerashetty <167009823+bveeru123@users.noreply.github.com>
2024-05-23 20:32:04 +0530

    Day

--- Linear.java
+++ Linear.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class Binarysearch{
+public class Linearsearch{
     public static void main(String args[]) {
         Scanner sc=new Scanner(System.in );
     int [] arr={4,3,6,8,6,1,24,6};
